5461632f0ba0c518b080d256b2b3541ddb224c4e
advika710 <advikajk7@gmail.com>
2024-08-23 11:04:57 +0530

    Add files via upload

---
+++ python_day2_operator_assignment_3.py
@@ -0,0 +1,11 @@
+#Python program to convert the temperature in degree centigrade to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit.")
+
+"""
+Output:
+Enter temperature in Celsius: 30
+30.0 degree Celsius is equal to 86.0 degree Fahrenheit.
+"""
