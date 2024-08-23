2d705f9ac66d48d2f4fccaf58ab9acb526b07442
advika710 <advikajk7@gmail.com>
2024-08-23 11:04:35 +0530

    Add files via upload

---
+++ python_day2_operator_assignment_1.py
@@ -0,0 +1,16 @@
+#Calculate the multiplication and sum of two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+multiplication = num1 * num2
+summation = num1 + num2
+print(f"Multiplication of {num1} and {num2} is: {multiplication}")
+print(f"Sum of {num1} and {num2} is: {summation}")
+
+"""
+Output:
+Enter first number: 15
+Enter second number: 8
+Multiplication of 15 and 8 is: 120
+Sum of 15 and 8 is: 23
+"""
