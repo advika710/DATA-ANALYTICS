ee2f21e5ae0522bc0025eafe48fe6fde3873ac04
advika710 <advikajk7@gmail.com>
2024-08-28 17:28:20 +0530

    Add files via upload

---
+++ Python_Day5_Function_Assignment4.py
@@ -0,0 +1,16 @@
+#Q4 Accept a name from the user and display that in lower case using lower() function.
+
+# Accept a name from the user
+name = input("Enter your name: ")
+
+# Convert the name to lower case
+lower_case_name = name.lower()
+
+# Print the name in lower case
+print("Your name in lower case is:", lower_case_name)
+
+"""
+Output:
+Enter your name: Advika
+Your name in lower case is: advika
+"""
