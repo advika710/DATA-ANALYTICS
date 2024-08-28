250dce6a15af5b3396bc0acadeba6e3da5e45809
advika710 <advikajk7@gmail.com>
2024-08-28 17:27:44 +0530

    Add files via upload

---
+++ Python_Day5_Function_Assignment1.py
@@ -0,0 +1,18 @@
+#Q1 Declare a div() function with two parameters. Then call the function and pass two numbers and display their division.
+
+# Define the function div with two parameters a and b
+def div(a, b):
+    # Perform division and return the result
+    return a / b
+
+# Call the function with two numbers and store the result
+result = div(12, 2)
+
+# Print the result of the division
+print("The result of division is:", result)
+
+
+"""
+output:
+The result of division is: 6.0
+"""
