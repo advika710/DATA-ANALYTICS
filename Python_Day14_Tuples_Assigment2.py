6aded0dfb2032f3a5d4fb9f04fbe593639492b2c
advika710 <advikajk7@gmail.com>
2024-09-12 11:48:55 +0530

    Add files via upload

---
+++ Python_Day14_Tuples_Assigment2.py
@@ -0,0 +1,14 @@
+#Q2. Write a Python program to convert a list to a tuple.
+#    Input: listx = [5, 10, 7, 4, 15, 3]
+
+# Define the list with given elements
+listx = [5, 10, 7, 4, 15, 3]
+
+# Use the tuple() function to convert the list to a tuple
+tuplex = tuple(listx)
+
+# Print the result
+print("The converted tuple is:", tuplex)
+
+#ANS=> listx = [5, 10, 7, 4, 15, 3]
+#ANS=> The converted tuple is: (5, 10, 7, 4, 15, 3)
